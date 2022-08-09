9419a1b437d2a4d2ab643d1bc0c39a6e3ba2cb76
Daria <fedu@inbox.ru>
2022-08-10 00:13:03 +0300

    Homework 06 done

--- Homework06/Program.cs
+++ Homework06/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 //0, 7, 8, -2, -2 -> 2
 //1, -7, 567, 89, 223-> 3
-
+/*
 int Count (int [] array)
 {
     int result = 0;
@@ -38,6 +38,40 @@ void ShowArray (int [] array)
 int [] array1 = NewArray(size);
 ShowArray(array1);
 Console.Write($"Введено {Count(array1)} элементов больше 0");
+*/
+//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
 
-//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//значения b1, k1, b2 и k2 задаются пользователем.
 //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+
+
+
+
+double XCross (double b1, double b2, double k1, double k2)
+{
+    double x = ( b2 - b1 ) / ( k1 - k2);
+    return x;
+}
+
+double YCross (double x, double b1, double k1)
+{
+    double y = k1 * x + b1;
+    return y;
+}
+
+
+Console.WriteLine("Две прямые заданы уравнениями y = k1 * x + b1, y = k2 * x + b2");
+Console.Write("Введите значение b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+double x = XCross (b1, b2, k1, k2);
+double y = YCross(x, b1, k1);
+Console.WriteLine($"Точка пересечения этих приямых - ({x}, {y})");
+
